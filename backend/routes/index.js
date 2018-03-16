4cd24332a350637b694eb997b886b4e301fe208f
FTeddy <ainsophoure@gmail.com>
2018-03-16 13:28:01 +0700

    edited index start

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,9 @@ const {authlogin} = require('../middleware/auth')
 const {getUserDetail} = require('../controllers/user.controller');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({
+    message: 'hello'
+  })
 });
 
 module.exports = router;
